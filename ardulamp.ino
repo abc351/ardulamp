9877761a24e4efd653f61eadc2d06d9ebe0f4ed0
abc351 <80473809+abc351@users.noreply.github.com>
2023-11-26 12:16:56 +0900

    Create ardulamp.ino

---
+++ ardulamp.ino
@@ -0,0 +1,24 @@
+void setup() {
+  pinMode(2,OUTPUT);
+  digitalWrite(2,1);
+  pinMode(4,OUTPUT);
+}
+int j;
+void loop() {
+  pinMode(3,INPUT);
+  digitalWrite(4,0);
+  delay(1);
+  for(int i=0;i<100;i++){
+    if(digitalRead(3)){
+      j=45; break;
+    }
+    delayMicroseconds(50);
+  }
+  if(--j>0){
+      pinMode(3,OUTPUT);
+      digitalWrite(4,1);
+      digitalWrite(3,0);
+  }
+  else j=0;
+  delay(20);
+}
