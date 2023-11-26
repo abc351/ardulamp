e8f162f2514b24f525dab3aff86614cf80929fb1
abc351 <80473809+abc351@users.noreply.github.com>
2023-11-26 12:36:20 +0900

    Update ardulamp.ino

--- ardulamp.ino
+++ ardulamp.ino
@@ -9,9 +9,7 @@ void loop() {
   digitalWrite(4,0);
   delay(1);
   for(int i=0;i<100;i++){
-    if(digitalRead(3)){
-      j=45; break;
-    }
+    if(digitalRead(3)){ j=15; break; }
     delayMicroseconds(50);
   }
   if(--j>0){
@@ -20,5 +18,5 @@ void loop() {
       digitalWrite(3,0);
   }
   else j=0;
-  delay(20);
+  delay(15);
 }
